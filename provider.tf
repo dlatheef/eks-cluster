1aebfcdc1872dc2cc051125f4450b831b14bf19a
latheef-desumukh <80037253+dlatheef@users.noreply.github.com>
2024-12-27 15:32:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_version = ">= 1.3.0, < 2.0.0"  # Ensure compatibility with your Terraform version
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0.0"  # Define provider version constraints
-    }
-  }
-}
+
 provider "aws" {
   region  = "ap-south-1"
 #  profile = "latheef"
