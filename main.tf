9ed890ea25e36041e5d356c8cb84bd279f2ac284
latheef-desumukh <80037253+dlatheef@users.noreply.github.com>
2024-12-27 15:32:51 +0530

    Update main.tf
    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0, < 2.0.0"  # Ensure compatibility with your Terraform version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"  # Define provider version constraints
+    }
+  }
+}
+
+
 
 module "eks_vpc" {
   source        = "./modules/vpc"
